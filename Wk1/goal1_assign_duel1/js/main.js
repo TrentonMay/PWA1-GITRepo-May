6d9d6ea765800900a95aa2f48982ae382c791a8d
TrentonMay <TrentonMay@fullsail.edu>
2015-02-04 22:35:31 -0500

    Created variables for the players

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -1,3 +1,15 @@
-/**
- * Created by Trent on 2/4/15.
- */
+// Trenton May
+// 4/2/2015
+// Analyze duel
+
+var player1 = "SwagbobYoloPants";
+var player2 = "Lieutenant Dan";
+
+var player1hp = 100;
+var player2hp = 100;
+
+var player1dam = 13;
+var player2dam = 15;
+
+
+
