dc657d2419648bea952a37dc9dab8fe9747733c6
TrentonMay <TrentonMay@fullsail.edu>
2015-02-04 19:39:30 -0500

    Completed 1st and second activities

--- Wk1/goal1_basics/js/main.js
+++ Wk1/goal1_basics/js/main.js
@@ -195,6 +195,10 @@ console.log('------ arrays ----------');
  */
 
     //arrays can hold any data type
+    console.log("index 0:", myArr[0]);
+    console.log("index 1:", myArr[1]);
+    console.log("index 2:", myArr[2]);
+    console.log("index 3:", myArr[3]);
 
 
 
@@ -207,7 +211,8 @@ console.log('------ arrays ----------');
 
  ********************************************/
 
-
+    var array1 = [10, 50, 100, 200];
+    console.log(array1[0] + array1[1] + array1[2] + array1[3]);
 
 /*******************************************
 
@@ -223,6 +228,12 @@ console.log('------ arrays ----------');
     3. output array items using string concatenation
 
  ********************************************/
+
+    var myArray = new Array();
+    myArray[0] = "baseball";
+    myArray[1] = "football";
+    myArray[2] = "basketball";
+    console.log(myArray[0] + "," + myArray[2] + "," + myArray[3]);
 
 
  // conditionals: (definition, indexing, math, if, if-else, if-else if, ternary)
