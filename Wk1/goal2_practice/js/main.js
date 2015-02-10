2ac1b3517c12fa7cdbd3e85202ab055cecd47818
TrentonMay <TrentonMay@fullsail.edu>
2015-02-09 19:34:20 -0500

    Completed array practice

--- Wk1/goal2_practice/js/main.js
+++ Wk1/goal2_practice/js/main.js
@@ -275,34 +275,48 @@ var arr3 = ['apple', 'orange', 50393, 7.324];
 // indexOf()
 //Returns the first (least) index of an element within the array equal
 //to the specified value, or -1 if none is found.
-
+	console.log(arr2.indexOf("c"));
 
 // join()
 // Joins all elements of an array into a string.
-
+	console.log(arr2.join(","));
 
 // pop()
 // Removes the last element from an array and returns that element
 // shift() removes the first element from an array.
+	console.log(arr1.pop());
+	console.log(element);
+	console.log(arr1);
 
 
 // push()
 // Adds one or more elements to the end of an array and returns the new
 //   length of the array
 // unshift() - Adds one or more elements to the front of an array.
-
+	arr2.push("f");
+	console.log(arr2);
 
 // reverse()
 // Reverses the order of the elements of an array
+	console.log(arr1.reverse());
 
 
 // splice()
 // Adds and/or removes elements from an array.
+	console.log(arr1);
+	console.log(arr1.splice(4,2));
+	console.log(arr1);
 
 
 // forEach()
 // Calls a function for each element in the array.
+	console.log(arr1);
+	arr1.forEach(function(element, index, array){
+		console.log("element" , element);
+		console.log("index", index);
+		console.log("array", array);
 
+	});
 
 
 
@@ -326,6 +340,19 @@ var arr3 = ['apple', 'orange', 50393, 7.324];
     var secondQtr = ["Apr", "May", "Jun"];
     var secondHalfYr = ["Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
     var combined;
+
+	var combined = firstQtr.concat(secondQtr);
+	console.log("Concat 2 arrays", combined);
+
+	var combined = firstQtr.concat(secondQtr, secondHalfYr);
+	console.log("Concat 3 arrays", combined);
+
+	console.log("Joined arrays:", combined.join());
+	console.log("Took out", combined.pop());
+	console.log("Show array:", combined);
+
+	var results = secondHalfYr.slice(2,4);
+	console.log("slice:", results);
 
 
 /*
@@ -468,7 +495,7 @@ console.log('------ MORE Functions ----------');
 	*/
 
 	var fn = function(){
-		
+
 	})();
 
 
