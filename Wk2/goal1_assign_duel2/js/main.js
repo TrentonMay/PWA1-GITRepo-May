405b0c333dba7a5925ed2a7782e60e3a015cc8ad
TrentonMay <TrentonMay@fullsail.edu>
2015-02-11 22:37:04 -0500

    Updated duel to use arrays.
    Also added some comments

--- Wk2/goal1_assign_duel2/js/main.js
+++ Wk2/goal1_assign_duel2/js/main.js
@@ -2,6 +2,8 @@
 // 4/2/2015
 // Analyze duel
 
+
+/*  Commented this old section out. I kept it just in case if it becomes useful later
 var player1 = "SwagbobYoloPants"; //name of player 1
 var player2 = "Lieutenant Dan"; //name of player 2
 
@@ -10,34 +12,39 @@ var player2hp = 100;    //how much health player 2 has
 
 var player1dam = 20;    //max damage player 1 can do each round
 var player2dam = 22;    //max damage player 2 can do each round
+*/
+
 var winner = false; //used to see if winnerCheck returns a winner
 
+var fighter1 = ["SwagbobYoloPants", 100, 20]; //array for fighter 1. Contains name, health, and damage
+var fighter2 = ["Lieutenant Dan", 100, 22]; //array for fighter 2. Contains name, health, and damage
+
 function winnerCheck(round){    //this is the winnerCheck function. Checks if there is a winner each round
-    if(player1hp <= 0 && player2hp <= 0){   //this is says that if player1 and player2 die in the same round there is no winner
+    if(fighter1[1] <= 0 && fighter2[1] <= 0){   //this is says that if player1 and player2 die in the same round there is no winner
         alert("No Winner"); //lets the user know there is no winner since both players died
         winner = true;  //keeps the tied alert from running if there is actually a winner
         return true;    //ends the function and breaks the loop
 
-    }else if(player1hp <= 0){   //If player1 health reaches 0 then he is dead and player2 wins
-        alert("GAME OVER\n " + player2 + " Wins");  //tells the user player2 won
+    }else if(fighter1[1] <= 0){   //If player1 health reaches 0 then he is dead and player2 wins
+        alert("GAME OVER\n " + fighter2[0] + " Wins");  //tells the user player2 won
         winner = true;  //keeps the tied alert from running if there is actually a winner
         return true;    //ends the function and breaks the loop
 
-    }else if(player2hp <= 0){   //If player2 health reaches 0 then he is dead and player2 wins
-        alert("GAME OVER\n " + player1 + " Wins");
+    }else if(fighter2[1] <= 0){   //If player2 health reaches 0 then he is dead and player2 wins
+        alert("GAME OVER\n " + fighter1[0] + " Wins");  //alerts the user that player 1 wins
         winner = true;  //keeps the tied alert from running if there is actually a winner
         return true;    //ends the function and breaks the loop
 
     }else{  //If no one won yet then it runs this alert to say the round is over
-        alert(player1 + ":" + player1hp + "\nRound " + round + " Over \n" +  player2 + ":" + player2hp);
+        alert(fighter1[0] + ":" + fighter1[1] + "\nRound " + round + " Over \n" +  fighter2[0] + ":" + fighter2[1]);
     }
 }
 
 function fight(){   //this is the begining of the function that will loop
-    alert(player1 + ":" + player1hp + " \nSTART\n " +  player2 + ":" + player2hp);  //This alert runs before the loop and shows the health of the players
+    alert(fighter1[0] + ":" + fighter1[1] + " \nSTART\n " +  fighter2[0] + ":" + fighter2[1]);  //This alert runs before the loop and shows the health of the players
     for (var round = 1; round < 10; round++){   //adds up the round number after each loop
-        player1hp = player1hp - Math.floor(Math.random() * (player2dam - (player2dam / 2)) + (player2dam / 2)); //calculates the damage a player does each round
-        player2hp = player2hp - Math.floor(Math.random() * (player1dam - (player1dam / 2)) + (player1dam / 2)); //calculates the damage a player does each round
+        fighter1[1] = fighter1[1] - Math.floor(Math.random() * (fighter2[2] - (fighter2[2] / 2)) + (fighter2[2] / 2)); //calculates the damage a player does each round
+        fighter2[1] = fighter2[1] - Math.floor(Math.random() * (fighter1[2] - (fighter1[2] / 2)) + (fighter1[2] / 2)); //calculates the damage a player does each round
         if(winnerCheck(round)){ //calls on the winner check function. Also checks if winner has been found
             break; //breaks the loop if a winner has been found
         }
