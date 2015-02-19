e1f794d73b0dd43b404a799f407d90ae42201406
TrentonMay <TrentonMay@fullsail.edu>
2015-02-18 20:59:34 -0500

    Completed duel code

--- Wk3/goal4_assign_duel3/js/main.js
+++ Wk3/goal4_assign_duel3/js/main.js
@@ -20,12 +20,13 @@ var winner = false; //used to see if winnerCheck returns a winner
 //var fighter2 = ["Lieutenant Dan", 100, 22]; //array for fighter 2. Contains name, health, and damage
 
 fighter1={name:"SwagbobYoloPants", health:100, damage:20 };
-fighter2={name:"Lieutenant Dan", health:100, damage:22 };
+fighter2={name:"Lieutenant Dan", health:100, damage:20 };
 
 var nameSelect1 = document.getElementById("kabal");
 var nameSelect2 = document.getElementById("kratos");
 var roundSelect = document.getElementById("round");
 var scores = document.getElementById("scores");
+var round = 0;
 
 function winnerCheck(){    //this is the winnerCheck function. Checks if there is a winner each round
     if(fighter1.health <= 0 && fighter2.health <= 0){   //this says that if player1 and player2 die in the same round there is no winner
@@ -46,11 +47,17 @@ function winnerCheck(){    //this is the winnerCheck function. Checks if there i
         winner = true;  //keeps the tied alert from running if there is actually a winner
         return true;    //ends the function and breaks the loop
 
-    //}else{  //If no one won yet then it runs this alert to say the round is over
+    }else{  //If no one won yet then it runs this alert to say the round is over
         //alert(fighter1.name + ":" + fighter1.health + "\nRound " + round + " Over \n" +  fighter2.name + ":" + fighter2.health);
+        nameSelect1.innerHTML = "<p>" + fighter1.name + ": " + fighter1.health + "</p>";
+        nameSelect2.innerHTML = "<p>" + fighter2.name + ": " + fighter2.health + "</p>";
+        roundSelect.innerHTML = "<p>Round " + round + "</p>";
 
-    //}
+        if(round == 10){
+            scores.innerHTML = "<p> It's a Tie</p>";
+        }
     }
+
 }
 
 document.getElementById("fight_box").onclick = function(e){
@@ -58,23 +65,21 @@ document.getElementById("fight_box").onclick = function(e){
     fight();
     e.preventDefault();
     return false;
+
 };
 
 function fight(){   //this is the beginning of the function that will loop
-    //alert(fighter1.name + ":" + fighter1.health + " \nSTART\n " +  fighter2.name + ":" + fighter2.health);  //This alert runs before the loop and shows the health of the players
+        round++;
+        if(round >10 || winner == true){
+            return false;
+        }
 
         //below is the damage counter
         fighter1.health = fighter1.health - Math.floor(Math.random() * (fighter2.damage - (fighter2.damage / 2)) + (fighter2.damage / 2)); //calculates the damage a player does each round
         fighter2.health = fighter2.health - Math.floor(Math.random() * (fighter1.damage - (fighter1.damage / 2)) + (fighter1.damage / 2)); //calculates the damage a player does each round
 
-        if(winnerCheck()){ //Calls on the winnerCheck function so that we may see if a winner has been found.
-
-        }
-
-        if(winner == false){    //checks to see if both players lived after 10 rounds. If they did then it alerts them that it's a tie
-            //alert("It's a tie");
-            scores.innerHTML = "<p> It's a tie</p>";
-    }
+        winnerCheck(); //Calls on the winnerCheck function so that we may see if a winner has been found.
+    
 }
 
 //fight(); //calls on the fight function to begin
